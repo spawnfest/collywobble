da89cf07cecd0f1f736955cbdbb225f5b4899d58
goto-engineering <goto@gotoengineering.io>
2022-10-15 15:09:19 -0500

    Comment out doctor PSQL check for now

--- .medic/doctor.exs
+++ .medic/doctor.exs
@@ -3,5 +3,5 @@
   {Medic.Checks.Hex, :local_hex_installed?},
   {Medic.Checks.Hex, :local_rebar_installed?},
   {Medic.Checks.Hex, :packages_installed?},
-  {Medic.Checks.Postgres, :database_exists?, ["collywobble_dev"]}
+  # {Medic.Checks.Postgres, :database_exists?, ["collywobble_dev"]}
 ]
