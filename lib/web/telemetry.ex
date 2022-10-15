6d06001c4535897c17061c3ba1caa15d4bed5fef
Eric Saxby <sax@livinginthepast.org>
2022-10-14 17:57:07 -0700

    Formatter line length 120
    Co-authored-by: Goto Engineering <max@gotoengineering.io>

--- lib/web/telemetry.ex
+++ lib/web/telemetry.ex
@@ -49,8 +49,7 @@ defmodule Web.Telemetry do
       ),
       summary("collywobble.repo.query.idle_time",
         unit: {:native, :millisecond},
-        description:
-          "The time the connection spent waiting before being checked out for the query"
+        description: "The time the connection spent waiting before being checked out for the query"
       ),
 
       # VM Metrics
