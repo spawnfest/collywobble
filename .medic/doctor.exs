fbe837e9b121a40b5098d3b4645327b6fdd1c3c5
Eric Saxby <sax@livinginthepast.org>
2022-10-14 17:32:49 -0700

    Add database_exists? check to doctor

--- .medic/doctor.exs
+++ .medic/doctor.exs
@@ -2,5 +2,6 @@
   {Medic.Checks.Homebrew, :bundled?},
   {Medic.Checks.Hex, :local_hex_installed?},
   {Medic.Checks.Hex, :local_rebar_installed?},
-  {Medic.Checks.Hex, :packages_installed?}
+  {Medic.Checks.Hex, :packages_installed?},
+  {Medic.Checks.Postgres, :database_exists?, ["collywobble_dev"]}
 ]
