e4312264ed652edc0911aa509b6ae9866b962560
Eric Saxby <sax@livinginthepast.org>
2022-10-15 23:23:47 -0700

    Add postgres checks back in to doctor

--- .medic/doctor.exs
+++ .medic/doctor.exs
@@ -3,5 +3,6 @@
   {Medic.Checks.Hex, :local_hex_installed?},
   {Medic.Checks.Hex, :local_rebar_installed?},
   {Medic.Checks.Hex, :packages_installed?},
-  # {Medic.Checks.Postgres, :database_exists?, ["collywobble_dev"]}
+  {Medic.Checks.Postgres, :role_exists?, ["postgres"]},
+  {Medic.Checks.Postgres, :database_exists?, ["collywobble_dev", username: "postgres"]}
 ]
